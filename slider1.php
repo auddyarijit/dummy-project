ec7c1d4bdc83d8d095f04834c8d92e5280ab30fa
auddyarijit <arijit.a@techsumsolution.com>
2023-02-03 22:12:45 +0530

    1st change

--- slider1.php
+++ slider1.php
@@ -90,7 +90,7 @@ img{
     <span class="carousel-control-next-icon"></span>
   </a>
 </div>
-
+ok new line
 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
